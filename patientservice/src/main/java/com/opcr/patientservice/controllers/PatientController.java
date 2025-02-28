3d3243f6be8e0c6fecc00abd2039ab429b0e7974
DAC00 <boiteaspamv2@gmail.com>
2025-02-28 08:51:33 +0100

    add Controller and fix id for Patient

---
+++ patientservice/src/main/java/com/opcr/patientservice/controllers/PatientController.java
@@ -0,0 +1,46 @@
+package com.opcr.patientservice.controllers;
+
+import com.opcr.patientservice.models.Patient;
+import com.opcr.patientservice.services.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/patient")
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping("/all")
+    public List<Patient> getPatients() {
+        return patientService.getPatients();
+    }
+
+    @GetMapping("/{id}")
+    public Patient getPatient(@PathVariable("id") Integer idPatient) {
+        Optional<Patient> patient = patientService.getPatient(idPatient);
+        return patient.orElse(null);
+    }
+
+    @PutMapping("/update/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updatePatient(@PathVariable("id") Integer idPatient, @RequestBody Patient patient) {
+        patientService.updatePatient(idPatient, patient);
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addPatient(@RequestBody Patient patient){
+        patientService.addPatient(patient);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deletePatient(@PathVariable("id") Integer idPatient){
+        patientService.deletePatient(idPatient);
+    }
+}
--- patientservice/src/main/java/com/opcr/patientservice/models/Patient.java
+++ patientservice/src/main/java/com/opcr/patientservice/models/Patient.java
@@ -1,6 +1,8 @@
 package com.opcr.patientservice.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -11,6 +13,7 @@ import java.util.Date;
 public class Patient {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String lastName;

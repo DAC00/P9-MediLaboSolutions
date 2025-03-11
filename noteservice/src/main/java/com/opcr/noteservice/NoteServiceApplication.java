61ec6a83dbbe6884b4e99e9b070437e69ebdd365
DAC00 <boiteaspamv2@gmail.com>
2025-03-11 13:30:54 +0100

    init data for Note db

--- noteservice/src/main/java/com/opcr/noteservice/NoteServiceApplication.java
+++ noteservice/src/main/java/com/opcr/noteservice/NoteServiceApplication.java
@@ -1,13 +1,40 @@
 package com.opcr.noteservice;
 
+import com.opcr.noteservice.models.Note;
+import com.opcr.noteservice.repositories.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NoteServiceApplication {
+public class NoteServiceApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NoteServiceApplication.class, args);
-	}
+    @Autowired
+    private NoteRepository noteRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(NoteServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Init.");
+
+        noteRepository.deleteAll();
+        System.out.println("Reset db.");
+
+        noteRepository.save(new Note(1,1,"TestNone","Le patient déclare qu'il 'se sent très bien' Poids égal ou inférieur au poids recommandé."));
+        noteRepository.save(new Note(2,2,"TestBorderline","Le patient déclare qu'il ressent beaucoup de stress au travail Il se plaint également que son audition est anormale dernièrement."));
+        noteRepository.save(new Note(3,2,"TestBorderline","Le patient déclare avoir fait une réaction aux médicaments au cours des 3 derniers mois Il remarque également que son audition continue d'être anormale."));
+        noteRepository.save(new Note(4,3,"TestInDanger","Le patient déclare qu'il fume depuis peu."));
+        noteRepository.save(new Note(5,3,"TestInDanger","Le patient déclare qu'il est fumeur et qu'il a cessé de fumer l'année dernière Il se plaint également de crises d’apnée respiratoire anormales Tests de laboratoire indiquant un taux de cholestérol LDL élevé."));
+        noteRepository.save(new Note(6,4,"TestEarlyOnset","Le patient déclare qu'il lui est devenu difficile de monter les escaliers Il se plaint également d’être essoufflé Tests de laboratoire indiquant que les anticorps sont élevés Réaction aux médicaments."));
+        noteRepository.save(new Note(7,4,"TestEarlyOnset","Le patient déclare qu'il a mal au dos lorsqu'il reste assis pendant longtemps."));
+        noteRepository.save(new Note(8,4,"TestEarlyOnset","Le patient déclare avoir commencé à fumer depuis peu Hémoglobine A1C supérieure au niveau recommandé."));
+        noteRepository.save(new Note(9,4,"TestEarlyOnset","Taille, Poids, Cholestérol, Vertige et Réaction."));
+
+        noteRepository.findAll().forEach(note -> System.out.println(note.toString()));
+        System.out.println("Init db complete.");
+    }
 }

2b36c0a96a11aef59a0dbc6af060fc44cc5cc1a1
DAC00 <boiteaspamv2@gmail.com>
2025-03-11 17:04:00 +0100

    init Risque service

---
+++ risqueservice/src/main/java/com/opcr/risqueservice/RisqueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.opcr.risqueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisqueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisqueServiceApplication.class, args);
+	}
+
+}
---
+++ risqueservice/src/test/java/com/opcr/risqueservice/RisqueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opcr.risqueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RisqueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

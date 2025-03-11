27b5572ff017ce01ffed4cdaf4cd0e57f1e50a4e
DAC00 <boiteaspamv2@gmail.com>
2025-03-11 11:41:34 +0100

    rename Controller endpoint to /api/

--- patientservice/src/main/java/com/opcr/patientservice/controllers/PatientController.java
+++ patientservice/src/main/java/com/opcr/patientservice/controllers/PatientController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/patient")
+@RequestMapping("/api/patient")
 public class PatientController {
 
     @Autowired

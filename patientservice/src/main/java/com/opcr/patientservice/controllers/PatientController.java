125ddb66fecd6b6146359590a7e74861f5c8ce6e
DAC00 <boiteaspamv2@gmail.com>
2025-03-13 18:48:27 +0100

    add doc to controller

--- patientservice/src/main/java/com/opcr/patientservice/controllers/PatientController.java
+++ patientservice/src/main/java/com/opcr/patientservice/controllers/PatientController.java
@@ -17,28 +17,56 @@ public class PatientController {
     @Autowired
     private PatientService patientService;
 
+    /**
+     * Get all the Patient.
+     *
+     * @return all the Patient.
+     */
     @GetMapping("/all")
     public List<Patient> getPatients() {
         return patientService.getPatients();
     }
 
+    /**
+     * Get the Patient with the idPatient.
+     *
+     * @param idPatient id of the Patient.
+     * @return a Patient.
+     */
     @GetMapping("/{id}")
     public Patient getPatient(@PathVariable("id") Integer idPatient) {
         Optional<Patient> patient = patientService.getPatient(idPatient);
         return patient.orElse(null);
     }
 
+    /**
+     * Get the birthdate and gender from a Patient with idPatient.
+     *
+     * @param idPatient id of the Patient.
+     * @return a PatientInfoDTO.
+     */
     @GetMapping("/info/{id}")
     public PatientInfoDTO getPatientBirthdateAndGender(@PathVariable("id") Integer idPatient) {
         return patientService.getPatientBirthdateAndGender(idPatient);
     }
 
+    /**
+     * Update the Patient with idPatient.
+     *
+     * @param idPatient id of the Patient.
+     * @param patient   Patient with updated data.
+     */
     @PutMapping("/update/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void updatePatient(@PathVariable("id") Integer idPatient, @RequestBody Patient patient) {
         patientService.updatePatient(idPatient, patient);
     }
 
+    /**
+     * Save a new Patient into the database.
+     *
+     * @param patient to save.
+     */
     @PostMapping("/add")
     @ResponseStatus(HttpStatus.CREATED)
     public void addPatient(@RequestBody Patient patient) {

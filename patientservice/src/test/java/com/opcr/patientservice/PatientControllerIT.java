0425284e87b3a1b9edb39e4cdb83081b7d800718
DAC00 <boiteaspamv2@gmail.com>
2025-03-22 16:24:30 +0100

    patientService : update IT test

--- patientservice/src/test/java/com/opcr/patientservice/PatientControllerIT.java
+++ patientservice/src/test/java/com/opcr/patientservice/PatientControllerIT.java
@@ -6,105 +6,165 @@ import com.opcr.patientservice.repositories.PatientRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
+import java.sql.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
 @TestPropertySource("/test.properties")
+@AutoConfigureWebTestClient
 public class PatientControllerIT {
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebTestClient webTestClient;
 
     @Autowired
     private PatientRepository patientRepository;
 
     @Test
     public void getPatientsTest() throws Exception {
-        this.mockMvc.perform(get("/patient/all")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].lastName").value("One"))
-                .andExpect(jsonPath("$[1].lastName").value("Two"))
-                .andExpect(jsonPath("$[2].lastName").value("Three"))
-                .andExpect(jsonPath("$[3].lastName").value("Four")
-                );
+        webTestClient.get()
+                .uri("/api/patient/all")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK)
+                .expectBodyList(Patient.class)
+                .consumeWith(response -> {
+                    List<Patient> patients = response.getResponseBody();
+                    assertNotNull(patients);
+                    assertEquals(4, patients.size());
+                });
     }
 
     @Test
-    public void getPatientFourTest() throws Exception {
-        this.mockMvc.perform(get("/patient/4")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath(".id").value(4))
-                .andExpect(jsonPath(".lastName").value("Four"))
-                .andExpect(jsonPath(".firstName").value("Test"))
-                .andExpect(jsonPath(".birthdate").value("2002-06-02"))
-                .andExpect(jsonPath(".gender").value("F"))
-                .andExpect(jsonPath(".address").value("4 Valley Dr"))
-                .andExpect(jsonPath(".phoneNumber").value("400-555-6666")
-                );
+    public void getPatientOkTest() throws Exception {
+        webTestClient.get()
+                .uri("/api/patient/4")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK)
+                .expectBody(Patient.class)
+                .consumeWith(response -> {
+                    Patient patient = response.getResponseBody();
+                    assertNotNull(patient);
+                    assertEquals(4, patient.getId());
+                    assertEquals("Four", patient.getLastName());
+                    assertEquals("Test", patient.getFirstName());
+                    assertEquals("2002-06-02", patient.getBirthdate().toString());
+                    assertEquals("F", patient.getGender());
+                    assertEquals("4 Valley Dr", patient.getAddress());
+                    assertEquals("400-555-6666", patient.getPhoneNumber());
+                });
     }
 
     @Test
-    public void updatePatientOneTest() throws Exception {
-        String content = "{\"id\":1," +
-                "\"lastName\":\"Updated\"," +
-                "\"firstName\":\"Test\"," +
-                "\"birthdate\":\"1966-12-03\"," +
-                "\"gender\":\"F\"," +
-                "\"address\":\"1 Brookside St\"," +
-                "\"phoneNumber\":\"555-666-7777\"}";
+    public void getPatientNotFundTest() throws Exception {
+        int idPatientWhoDoesNotExist = 100;
+        webTestClient.get()
+                .uri("/api/patient/" + idPatientWhoDoesNotExist)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.NOT_FOUND)
+                .expectBody(Patient.class)
+                .consumeWith(response -> {
+                    Patient patient = response.getResponseBody();
+                    assertNull(patient);
+                    ;
+                });
+    }
 
-        this.mockMvc.perform(put("/patient/update/1")
+    @Test
+    public void updatePatientOkTest() throws Exception {
+        int idPatient = 2;
+        Patient patientToSave = new Patient();
+        patientToSave.setId(idPatient);
+        patientToSave.setLastName("Two");
+        patientToSave.setFirstName("Test");
+        patientToSave.setBirthdate(Date.valueOf("1945-06-02"));
+        patientToSave.setGender("M");
+        patientToSave.setAddress("test test");
+        patientToSave.setPhoneNumber("111-222-5555");
+
+        webTestClient.put()
+                .uri("/api/patient/update/" + idPatient)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(content)
-        );
+                .body(BodyInserters.fromValue(patientToSave))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.OK)
+                .expectBody(Patient.class)
+                .consumeWith(response -> {
+                    Patient patientSaved = response.getResponseBody();
+                    assertNotNull(patientSaved);
+                    assertEquals(patientToSave.getId(), patientSaved.getId());
+                    assertEquals(patientToSave.getLastName(), patientSaved.getLastName());
+                    assertEquals(patientToSave.getFirstName(), patientSaved.getFirstName());
+                    assertEquals(patientToSave.getGender(), patientSaved.getGender());
+                    assertEquals(patientToSave.getBirthdate().toString(), patientSaved.getBirthdate().toString());
+                    assertEquals(patientToSave.getAddress(), patientSaved.getAddress());
+                    assertEquals(patientToSave.getPhoneNumber(), patientSaved.getPhoneNumber());
+                });
+    }
 
-        Patient patient = patientRepository.findById(1).orElse(null);
+    @Test
+    public void updatePatientNotFoundTest() throws Exception {
+        int idPatientWhoDoesNotExist = 100;
+        Patient patientToSave = new Patient();
+        patientToSave.setId(idPatientWhoDoesNotExist);
+        patientToSave.setLastName("Two");
+        patientToSave.setFirstName("Test");
+        patientToSave.setBirthdate(Date.valueOf("1945-06-02"));
+        patientToSave.setGender("M");
 
-        assertNotNull(patient);
-        assertEquals("Updated", patient.getLastName());
-        assertEquals("555-666-7777", patient.getPhoneNumber());
-        assertEquals("1 Brookside St", patient.getAddress());
+        webTestClient.put()
+                .uri("/api/patient/update/" + idPatientWhoDoesNotExist)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(patientToSave))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.NOT_FOUND)
+                .expectBody(Patient.class)
+                .consumeWith(response -> {
+                    Patient patient = response.getResponseBody();
+                    assertNull(patient);
+                });
     }
 
     @Test
     public void addPatientTest() throws Exception {
-        String content = "{\"lastName\":\"New\"," +
-                "\"firstName\":\"Test\"," +
-                "\"birthdate\":\"1999-10-13\"," +
-                "\"gender\":\"F\"," +
-                "\"address\":\"New St\"," +
-                "\"phoneNumber\":\"444-222-7777\"}";
+        Patient patientToSave = new Patient();
+        patientToSave.setLastName("Test");
+        patientToSave.setFirstName("BOB");
+        patientToSave.setBirthdate(Date.valueOf("1966-12-03"));
+        patientToSave.setGender("M");
+        patientToSave.setAddress("test Street");
+        patientToSave.setPhoneNumber("111-222-5555");
 
-        this.mockMvc.perform(post("/patient/add")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(content))
-                .andExpect(status().isCreated());
-
-        List<Patient> patients = patientRepository.findAll();
-        Patient newPatient = patients.getLast();
-
-        assertEquals(5, patients.size());
-        assertEquals("New", newPatient.getLastName());
-        assertEquals("Test", newPatient.getFirstName());
-        assertEquals("1999-10-13", newPatient.getBirthdate().toString());
-        assertEquals("F", newPatient.getGender());
-        assertEquals("New St", newPatient.getAddress());
-        assertEquals("444-222-7777", newPatient.getPhoneNumber());
+        webTestClient.post()
+                .uri("/api/patient/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(patientToSave))
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.CREATED)
+                .expectBody(Patient.class)
+                .consumeWith(response -> {
+                    Patient patientSaved = response.getResponseBody();
+                    assertNotNull(patientSaved);
+                    assertNotNull(patientSaved.getId());
+                    assertEquals(patientToSave.getLastName(), patientSaved.getLastName());
+                    assertEquals(patientToSave.getFirstName(), patientSaved.getFirstName());
+                    assertEquals(patientToSave.getGender(), patientSaved.getGender());
+                    assertEquals(patientToSave.getBirthdate().toString(), patientSaved.getBirthdate().toString());
+                    assertEquals(patientToSave.getAddress(), patientSaved.getAddress());
+                    assertEquals(patientToSave.getPhoneNumber(), patientSaved.getPhoneNumber());
+                });
     }
 }

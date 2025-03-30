0bc4c044d320af0473f8fe5cd7645714f2bf8d9a
DAC00 <boiteaspamv2@gmail.com>
2025-03-30 18:54:32 +0200

    PatientService : cleaning test.

--- patientservice/src/test/java/com/opcr/patientservice/PatientControllerIT.java
+++ patientservice/src/test/java/com/opcr/patientservice/PatientControllerIT.java
@@ -79,7 +79,6 @@ public class PatientControllerIT {
                 .consumeWith(response -> {
                     Patient patient = response.getResponseBody();
                     assertNull(patient);
-                    ;
                 });
     }
 
